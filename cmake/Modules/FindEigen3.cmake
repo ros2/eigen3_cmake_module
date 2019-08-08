aee06cb60bc841cc3f031be4a657168d3115d8c8
Shane Loretz <sloretz@openrobotics.org>
2019-08-08 16:28:38 -0700

    Special case for eigen chocolatey package (#3)
    * Special case for eigen chocolatey package
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * SO snippet to see all variables
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Use definitions from 3.3.4 chocolatey package
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- cmake/Modules/FindEigen3.cmake
+++ cmake/Modules/FindEigen3.cmake
@@ -18,27 +18,37 @@
 include(FindPackageHandleStandardArgs)
 
 # Use Eigen3Config.cmake to do most of the work
-find_package(Eigen3 CONFIG)
+find_package(Eigen3 CONFIG QUIET)
 
-find_package_handle_standard_args(
-  Eigen3
-  VERSION_VAR
-    EIGEN3_VERSION_STRING
-  REQUIRED_VARS
-    EIGEN3_FOUND
-)
-if(EIGEN3_FOUND)
-  # Set standard variable names
-  # https://cmake.org/cmake/help/v3.5/manual/cmake-developer.7.html#standard-variable-names
-  set(_standard_vars
-    INCLUDE_DIRS
-    DEFINITIONS
-    ROOT_DIR)
-  foreach(_suffix ${_standard_vars})
-    set(_wrong_var "EIGEN3_${_suffix}")
-    set(_right_var "Eigen3_${_suffix}")
-    if(NOT DEFINED ${_right_var} AND DEFINED ${_wrong_var})
-      set(${_right_var} "${${_wrong_var}}")
-    endif()
-  endforeach()
+if(Eigen3_FOUND AND NOT EIGEN3_FOUND)
+  # Special case for Eigen 3.3.4 chocolately package
+  find_package_handle_standard_args(
+    Eigen3
+    VERSION_VAR
+      Eigen3_VERSION
+    REQUIRED_VARS
+      Eigen3_FOUND
+  )
+else()
+  find_package_handle_standard_args(
+    Eigen3
+    VERSION_VAR
+      EIGEN3_VERSION_STRING
+    REQUIRED_VARS
+      EIGEN3_FOUND
+  )
 endif()
+
+# Set standard variable names
+# https://cmake.org/cmake/help/v3.5/manual/cmake-developer.7.html#standard-variable-names
+set(_standard_vars
+  INCLUDE_DIRS
+  DEFINITIONS
+  ROOT_DIR)
+foreach(_suffix ${_standard_vars})
+  set(_wrong_var "EIGEN3_${_suffix}")
+  set(_right_var "Eigen3_${_suffix}")
+  if(NOT DEFINED ${_right_var} AND DEFINED ${_wrong_var})
+    set(${_right_var} "${${_wrong_var}}")
+  endif()
+endforeach()
